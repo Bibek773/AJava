196184d2a1e56b5644bb5ac293d84d5848fc625e
Bibek Ghimire <bibekghimire773@gmail.com>
2025-06-17 07:46:50 +0545

    Java url connection program

--- src/Ch4JavaNetworking/URLConnectionDemo.java
+++ src/Ch4JavaNetworking/URLConnectionDemo.java
@@ -1,4 +1,20 @@
 package Ch4JavaNetworking;
 
-public class URLConnectionDemo {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+
+class URLConnectionDemo {
+    public static void main(String[] args) throws Exception {
+        URL u = new URL("http://www.ghimire-bibek.com.np");
+        URLConnection con = u.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        while(true){
+            String line = br.readLine();
+            if(null==line){
+                break;
+            }
+            System.out.println(line);
+        }
+    }
 }
